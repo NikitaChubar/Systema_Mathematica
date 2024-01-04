d7f022a143a623f6da987ee4e28c007965e1fde1
Nikita Chubar <yachubar2015@gmail.com>
2024-01-04 18:09:10 +0500

    Configured the display of data from the database in the graph

--- systema_mathematica/workspace/views.py
+++ systema_mathematica/workspace/views.py
@@ -1,34 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import json
+from workspace.models import Article
 
 
 def graph(request):
-    nodes = [
-        {"id": 1, "label": "Node1"},
-        {"id": 2, "label": "Node2"},
-        {"id": 3, "label": "Node3"},
-        {"id": 4, "label": "Node4"},
-        {"id": 5, "label": "Node5"},
-        {"id": 6, "label": "Node6"},
-        {"id": 7, "label": "Node7"},
-        {"id": 8, "label": "Node8"},
-        {"id": 9, "label": "Node9"},
-        {"id": 10, "label": "Node10"},
-    ]
-
-    links = [
-        {"source": 5, "target": 10},
-        {"source": 5, "target": 4},
-        {"source": 6, "target": 2},
-        {"source": 5, "target": 6},
-        {"source": 2, "target": 4},
-        {"source": 8, "target": 7},
-        {"source": 10, "target": 9},
-        {"source": 10, "target": 1},
-        {"source": 4, "target": 3},
-        {"source": 7, "target": 4},
-    ]
+    articles = Article.objects.all()
+    nodes = []
+    links = []
+    for art in articles:
+        nodes.append({"id": art.pk, "label": art.title})
+        for source in art.required_articles.split():
+            links.append({"source": source, "target": art.pk})
 
     # Передайте данные в шаблон
     context = {
